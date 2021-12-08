321d7d1b2641432b66fdbb49b02360badc2e58b4
kulom001 <kulom007@gmail.com>
2021-12-08 14:28:47 -0800

    blue one op mode almost finished (needs to have more testing)

--- FtcRobotController-7.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue1OpMode.java
+++ FtcRobotController-7.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue1OpMode.java
@@ -25,15 +25,50 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 @Autonomous(name="AutoBlue1OpMode", group="AutoOpModes")
 public class AutoBlue1OpMode extends BaseAutoOpMode {
     private ElapsedTime runtime = new ElapsedTime();
+    String name = "AutoBlue1OpMode";
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap, true);
+
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready for run");
+        telemetry.addData(name, " Robot ready for run");
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+
+        driveForwardByInches(44, robot, DejaVuBot.TPS);
+        turnToPID(90,robot);
+        telemetry.addData(name, "Turned to hub  ");
+        telemetry.update();
+        driveForwardByInches(-6, robot, DejaVuBot.TPS);
+
+        //Drop the piece here and reset the arm to initial position
+        //robot.arm.moveArmToLevel(2);
+        //robot.arm.openBucketPos();
+        //robot.arm.closeBucketPos();
+        telemetry.addData(name, " Dropped the freight ");
+        telemetry.update();
+
+        //Move the robot to spin the duck
+        driveForwardByInches(40, robot, DejaVuBot.TPS);
+        turnToPID(-90,robot);
+        telemetry.addData(name, " Driving to wall ");
+        telemetry.update();
+
+        driveForwardByInches(-36, robot, DejaVuBot.TPS);
+        driveForwardByInches(-2, robot, DejaVuBot.TPS/2);
+        spinForOneDuck(robot, false);
+        turnToPID(90,robot);
+
+        telemetry.addData(name, " Duck spinned ");
+        telemetry.update();
+
+        driveForwardByInches(122, robot, DejaVuBot.TPS * 2);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(name, "Parked in warehouse");
+        telemetry.update();
     }
 }
