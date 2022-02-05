19ae3367f95496f14729c65bfe67dab6554fa7f9
kulom001 <67762782+kulom001@users.noreply.github.com>
2022-02-04 20:19:02 -0800

    added vision from past 2 weeks

---
+++ FtcRobotController-7.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnsTarget.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+public enum AnsTarget {
+    A,
+    B,
+    C,
+    D
+}
--- FtcRobotController-7.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAutonomousCode.java
+++ FtcRobotController-7.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAutonomousCode.java
@@ -1,2 +1,30 @@
-package org.firstinspires.ftc.teamcode;public class VisionAutonomousCode {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="VisionAutonomousCode", group="Auto")
+public class VisionAutonomousCode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        LevelChooser chooser = new LevelChooser(hardwareMap, telemetry);
+        waitForStart();
+        AnsTarget target = chooser.getTarget();
+        chooser.stop();
+        switch(target) {
+            case A:
+                telemetry.addData("ValueofTarget", "A");
+                telemetry.update();
+            case B:
+                telemetry.addData("ValueofTarget", "B");
+                telemetry.update();
+            case C:
+                telemetry.addData("ValueofTarget", "C");
+                telemetry.update();
+            case D:
+                telemetry.addData("ValueofTarget", "D");
+                telemetry.update();
+
+        }
+    }
 }
